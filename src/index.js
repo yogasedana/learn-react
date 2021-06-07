00e4776abd2c7d7922e50fcd43d81bcf3fc0494e
Yoga Sedana <yogasedana63@gmail.com>
2021-06-08 00:34:12 +0800

    belajar function di App.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,23 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+// pemanggilan sederhana ReactDOM
+// membuat variabel nama const nama
+const nama = "Gde Yoga Hary Sedana";
+// membuat variabel element yang didalmnya memanggil variabel nama dengan kurung {}
+const element = <h1>Hallo, Nama saya {nama} </h1>;
+// ReactDOM.render untuk mengerim element yang sudah dibuat, file dikirim menggunakan id
+ReactDOM.render(element, document.getElementById('salam'));
+
+// bisa munggunakan fungsi biasa
+function salam(){
+  // yang diisi dengan variabel nama const nama diatas
+  return "Hallo " + nama;
+}
+// sipanggil menggunakan variabel elements karna variabel element sudah di pakai di atas
+const elements = <h1>{salam(nama)}</h1>
+// lalau kirim ke html.index menggunakan id
+ReactDOM.render(elements, document.getElementById('halo'));
+
 reportWebVitals();
